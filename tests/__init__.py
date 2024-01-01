066650a8bdbbebd197fa5ff7ef161d10ae0bdd20
Connor Keane <connor.keane@kxnr.me>
2024-01-01 12:07:37 -0700

    root tests file

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,7 +5,4 @@ from satisfactory_tools.core.material import MaterialSpec, MaterialSpecFactory
 MATERIAL_NAMES = [chr(i) for i in range(97, 107)]
 
 Materials = MaterialSpecFactory(**{name: 0 for name in MATERIAL_NAMES})
-make_dataclass("Materials",
-                           [],
-                           bases=(MaterialSpec,), frozen=True)
 
